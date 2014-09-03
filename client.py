93fcda44a98fa5c0294c027b826de949940ff2ec
EchoSlam <jiran820@gmail.com>
2014-09-03 16:55:36 +0800

    initial

--- client.py
+++ client.py
@@ -5,3 +5,7 @@ Created on Wed Sep 03 16:17:58 2014
 @author: jiran
 """
 
+from gearman import GearmanClient
+client = GearmanClient(['127.0.0.1:4730'])
+request = client.submit_job('reverse_string','reverse this string')
+client.wait(86400)
